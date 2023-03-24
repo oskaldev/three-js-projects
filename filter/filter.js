debb546e54c0250a9f4788133a9b27a18f87e289
oskal314 <artem-osipovartem3002@mail.ru>
2023-03-24 20:07:53 +0300

    update

--- filter/filter.js
+++ filter/filter.js
@@ -126,3 +126,68 @@
 
 // setCategories();
 // setPrices();
+
+
+const data = [
+  {
+    id: 1,
+    name: "Кофе растворимый Bushido Original, стеклянная банка, 100 г",
+    img: "https://avatars.mds.yandex.net/get-mpic/4902598/img_id3380520081887581675.jpeg/600x800",
+    price: 335,
+    cat: "Растворимый",
+  },
+  {
+    id: 11,
+    name: "Кофе в зернах Fresco Arabica Blend, 1 кг",
+    img: "https://avatars.mds.yandex.net/get-mpic/5323357/img_id2638679866852579182.jpeg/600x800",
+    price: 616,
+    cat: "В зёрнах",
+  },
+  {
+    id: 2,
+    name: "Кофе растворимый Nescafe Gold сублимированный с добавлением молотого, пакет, 320 г",
+    img: "https://avatars.mds.yandex.net/get-mpic/4120716/img_id4199235586624124531.jpeg/600x800",
+    price: 357,
+    cat: "Растворимый",
+  },
+  {
+    id: 3,
+    name: "Кофе молотый Lavazza Crema e Gusto Classico, вакуумная упаковка, 250 г, вакуумная Superocean Heritage",
+    img: "https://m.media-amazon.com/images/I/61hGDiWBU8L._AC_UY879_.jpg",
+    price: 288,
+    cat: "Молотый",
+  },
+  {
+    id: 4,
+    name: "Кофе в капсулах Nescafe Dolce Gusto Каппучино 16 кап , 1 уп.",
+    img: "https://avatars.mds.yandex.net/get-mpic/4012462/img_id6564235945466849231.jpeg/600x800",
+    price: 516,
+    cat: "В капсулах",
+  },
+  {
+    id: 5,
+    name: "Кофе в зернах Senzaro Turino 1000 г, 100% арабика свежей обжарки",
+    img: "https://avatars.mds.yandex.net/get-mpic/4490717/img_id6928901970289948191.jpeg/600x800",
+    price: 996,
+    cat: "В зёрнах",
+  },
+];
+
+const productsContainer = document.querySelector(".products");
+const searchContainer = document.querySelector(".search");
+const categoriesContainer = document.querySelector(".categories");
+const priceRange = document.querySelector(".priceRange");
+const price = document.querySelector(".priceValue");
+
+const displayProducts = (filteredProducts) => {
+  productsContainer.innerHTML = filteredProducts.map((product) =>
+    `
+    <div class="product">
+    <img src="${product.img}" alt="${product.name}" />
+    <span class="name">${product.name}</span>
+    <span class="priceText">${product.price}р</span>
+    </div>
+    `
+  ).join('');
+}
+displayProducts(data);
